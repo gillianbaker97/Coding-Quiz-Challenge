44437e9caaf5dd7c6b5f5db9734ab6f15f71fbff
Gillian Baker <gillianbaker97@gmail.com>
2021-07-10 14:36:58 -0400

    new update

---
+++ script.js
@@ -0,0 +1,11 @@
+var buttonClick = document.querySelector("button");
+var firstPage = document.write("What is a HTML object?");
+var postFirstClic = document.write(".first page");
+
+buttonClick.addEventListener("click", function() {
+    var firstPage = document.write("What is a HTML object?");
+
+
+}
+
+)
