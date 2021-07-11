b9b7b70e8215ee6d5395adaf24a13c7de28c4931
Gillian Baker <gillianbaker97@gmail.com>
2021-07-11 11:14:11 -0400

    new update

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var buttonClick = document.getElementById("button");
 
 var Correct = document.querySelector('.Correct');
 var Incorrect = document.querySelector('.Incorrect');
+Correct.setAttribute("style", "opacity: 0; filter: alpha(opacity=0); display: flex; justify-content: center; color:grey; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 20px; " );
+Incorrect.setAttribute("style", "opacity: 0; filter: alpha(opacity=0); display: flex; justify-content: center; color:grey; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 20px; " );
 
 
 rightClick.addEventListener("click", function myFunction() {
